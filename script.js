87e37be3d9fd924ecf6be31849df87644812c55e
Michella Lockett <37868562+michellalockett@users.noreply.github.com>
2018-10-07 19:42:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+var friends = ['Mimi', 'Jen', 'Kim', 'John', 'Tim']
+
+for (var i = 0; i< friends.length; i ++) {
+    console.log('\n' + friends[i].toUpperCase() + ':');
+    for (var j = 99; j > 0; j--) {
+        if(j>2) {
+            console.log(" " + j + " lines of code in the file "+j+" lines of code, " + friends[i] + " strikes one out, clears it all out," + (j-1) + " more lines of code in the file.");
+        }else if(j === 2){
+            console.log(" " + j + " lines of code in the file " + j + " lines of code, " + friends[i] + " strikes one out, clears it all out," + (j-1) + " more line of code in the file.");
+        }else{
+           console.log(" " + j +  " line of code in the file, " + j + " line of code,  "+ friends[i]+  " strikes one out, clears it all out, no more lines of code in the file.");           
+        }
+    }
+}
